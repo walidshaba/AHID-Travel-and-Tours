0f7ca2803de376f08a32f6f1df076889f0afaf0b
Musa Shaba <musamohshaba@gmail.com>
2023-10-02 08:42:29 +0100

    Timely commit

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,5 +1,6 @@
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
 
-
-
-
-
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('active');
+});
